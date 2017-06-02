5769a67d1cf239625b5981cf09d145c9b4fbe547
alize <alize@jcommops.org>
2017-06-02 15:17:40 +0200

    removing unsupported characters

--- src/main/java/_int/wmo/def/opm/_2013/StatisticalFunctionCodeType.java
+++ src/main/java/_int/wmo/def/opm/_2013/StatisticalFunctionCodeType.java
@@ -15,13 +15,13 @@ import net.opengis.gml.v_3_2_1.ReferenceType;
 
 
 /**
- * The «CodeList» class 'StatisticalFunctionCode' specifies the type of statistical function
+ * The CodeList class 'StatisticalFunctionCode' specifies the type of statistical function
  *             that is applied to the base observable property to define the statistical summary;
  *             e.g. maximum air temperature.  Note that WMO provides two code-tables listing statistical
  *             operators:  - WMO No. 306 Vol I.2 Part B FM 92 GRIB code-table 4.10 'Type of statistical
  *             processing'; and - WMO No. 306 Vol I.2 Part B FM 94 BUFR code-table 0 08 023 'First-order
  *             statistics'  The GRIB2 code-table is defined as the 'recommended' vocabulary for this
- *             «CodeList» class but lacks some of the necessary terms. For example, the GRIB code-table
+ *             CodeList class but lacks some of the necessary terms. For example, the GRIB code-table
  *             includes 'Average' but does not include 'Mean', 'Mode' or 'Median' (which can be found
  *             in the BUFR code-table). However, the BUFR code-table is _NOT_ chosen because 'Accumulation'
  *             is entirely missing. Given that 'extensibility' is set to 'any', authors are free
