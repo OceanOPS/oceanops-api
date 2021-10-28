391bfacc3daab855b4ded5cf86c2a357d4084b1b
anthoninlize <alize@ocean-ops.org>
2021-10-28 11:10:05 +0200

    Updating ORM

---
+++ src/main/java/org/oceanops/api/orm/PtfAncillaryDevice.java
@@ -0,0 +1,9 @@
+package org.oceanops.api.orm;
+
+import org.oceanops.api.orm.auto._PtfAncillaryDevice;
+
+public class PtfAncillaryDevice extends _PtfAncillaryDevice {
+
+    private static final long serialVersionUID = 1L;
+
+}
