d08bea77e367eac3a65a81543d5e9d838b89f3e4
Anthonin Lizé <alize@ocean-ops.org>
2023-02-20 10:43:09 +0100

    docs: fixing TOC in documentation

--- src/main/webapp/scripts/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	var navSelector = '#toc';
-	var $myNav = $(navSelector);
-	Toc.init($myNav);
-	var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-	  target: navSelector
-	});
-});
